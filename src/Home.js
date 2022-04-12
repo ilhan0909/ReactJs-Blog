cb0dad6676e965d999f3e55e6124ffbf8945ea77
İlhan Bayramoğlu <bayramogluilhan09@gmail.com>
2022-04-12 17:49:49 +0300

    added dynamic age

--- src/Home.js
+++ src/Home.js
@@ -1,6 +1,16 @@
+function getAge(dateString) {
+    var today = new Date();
+    var birthDate = new Date(dateString);
+    var age = today.getFullYear() - birthDate.getFullYear();
+    var m = today.getMonth() - birthDate.getMonth();
+    if (m < 0 || (m === 0 && today.getDate() < birthDate.getDate())) {
+        age--;
+    }
+    return age;
+}
 const title = 'Welcome to my blog!';
 const department = 'Computer Engineer';
-const person = {name:'İlhan Bayramoğlu',age:23,country:'Turkey'};
+const person = {name:'İlhan Bayramoğlu',country:'Turkey'};
 const link = 'https://github.com/ilhan0909'
 
 const Home = () => {
@@ -8,7 +18,7 @@ const Home = () => {
         <div className="home">
         <h1>{title}</h1>
         <h2>I am a graduate {department} </h2>
-        <p>My name is {person.name}, i am {person.age} years old and i am from {person.country}.</p>
+        <p>My name is {person.name}, i am {getAge("1998/06/25")} years old and i am from {person.country}.</p>
         <a href={link}>Check My Github Profile</a>
         </div>
      );
