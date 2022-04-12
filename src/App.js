397dbf1bb936558cadb75f8f395d5acd42f76b6b
İlhan Bayramoğlu <bayramogluilhan09@gmail.com>
2022-04-12 17:03:52 +0300

    nested home and navbar,cleared main

--- src/App.js
+++ src/App.js
@@ -1,17 +1,15 @@
 import './App.css';
+import Navbar from './Navbar';
+import Home from './Home';
 
 function App() {
-  const title = 'Welcome to my blog!';
-  const department = 'Computer Engineer';
-  const person = {name:'İlhan Bayramoğlu',age:23,country:'Turkey'};
-  const link = 'https://github.com/ilhan0909'
+
   return (
     <div className="App">
+      <Navbar />
       <div className="content">
-        <h1>{title}</h1>
-        <h2>I am a graduate {department} </h2>
-        <p>My name is {person.name}, i am {person.age} years old and i am from {person.country}.</p>
-        <a href={link}>Check My Github Profile</a>
+        <Home />
+        
       </div>
     </div>
   );
