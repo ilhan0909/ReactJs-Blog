9419da540ccf399d86efdc6e7e96c857257a1e96
İlhan Bayramoğlu <bayramogluilhan09@gmail.com>
2022-04-14 18:44:26 +0300

    added clickable button and event hook

--- src/Home.js
+++ src/Home.js
@@ -1,3 +1,5 @@
+import { useState } from "react";
+
 function getAge(dateString) {
     var today = new Date();
     var birthDate = new Date(dateString);
@@ -14,14 +16,20 @@ const person = {name:'İlhan Bayramoğlu',country:'Turkey'};
 const link = 'https://github.com/ilhan0909';
 
 const Home = () => {
+    const [text, setText] = useState('Try button to see if it works?');
 
+    const handleClick = () => {
+        setText('Hook works on click!');
+    }
     
     return ( 
         <div className="home">
         <h1>{title}</h1>
         <h2>I am a graduate {department} </h2>
-        <p>My name is {person.name}, i am {getAge("1998/06/25")} years old and i am from {person.country}.</p>
+        <h3>My name is {person.name}, i am {getAge("1998/06/25")} years old and i am from {person.country}.</h3>
         <a href={link}>Check My Github Profile</a>
+        <h4>{text}</h4>
+        <button onClick={handleClick}>Click Me</button>
         </div>
      );
 }
