41d34c75a496f7e92e0de8925085773a84d13a59
İlhan Bayramoğlu <bayramogluilhan09@gmail.com>
2022-04-17 15:53:39 +0300

    created bloglist.js

---
+++ src/BlogList.js
@@ -0,0 +1,20 @@
+const BlogList = ({ blogs, title }) => {
+    // const blogs = props.blogs;
+    // const title = props.title;
+    // console.log(blogs);
+
+    return (
+        <div className="blog-list">
+            <h2>{title}</h2>
+            {blogs.map(blog => (
+                <div className="blog-preview" key={blog.id} >
+                    <h2>{blog.title}</h2>
+                    <h4>Written by {blog.author}</h4>
+                    <p>{blog.body}</p>
+                </div>
+            ))}
+        </div>
+    );
+}
+
+export default BlogList;
