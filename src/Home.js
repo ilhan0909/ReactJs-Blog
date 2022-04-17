4bd8076b397f424cfdb8444d3f4fc840214ffc2c
İlhan Bayramoğlu <bayramogluilhan09@gmail.com>
2022-04-17 15:20:25 +0300

    added mock blogs

--- src/Home.js
+++ src/Home.js
@@ -1,5 +1,4 @@
 import { useState } from "react";
-
 function getAge(dateString) {
     var today = new Date();
     var birthDate = new Date(dateString);
@@ -16,20 +15,26 @@ const person = {name:'İlhan Bayramoğlu',country:'Turkey'};
 const link = 'https://github.com/ilhan0909';
 
 const Home = () => {
-    const [text, setText] = useState('Try button to see if it works?');
-
-    const handleClick = () => {
-        setText('Hook works on click!');
-    }
+    const [blogs, setBlogs] = useState([
+      { title: 'My new website', body: 'MARIO WROTE THIS HEHEH', author: 'mario', id: 1 },
+      { title: 'Welcome party!', body: 'YOSHI ipsum...', author: 'yoshi', id: 2 },
+      { title: 'Web dev top tips', body: 'MARIO ipsum...', author: 'mario', id: 3 }
+    ])
     
     return ( 
         <div className="home">
+            
         <h1>{title}</h1>
         <h2>I am a graduate {department} </h2>
         <h3>My name is {person.name}, i am {getAge("1998/06/25")} years old and i am from {person.country}.</h3>
         <a href={link}>Check My Github Profile</a>
-        <h4>{text}</h4>
-        <button onClick={handleClick}>Click Me</button>
+        {blogs.map((blog) =>(
+                <div className="blog-preview" key={blog.id}>
+                    <h2>{ blog.title }</h2>
+                    <h4>Written by { blog.author }</h4>
+                    <p>{ blog.body }</p>
+                </div>
+            ))}
         </div>
      );
 }
