211c4e7aa488b4aa01e19d0c00b2e2345a0723ca
İlhan Bayramoğlu <bayramogluilhan09@gmail.com>
2022-04-18 09:19:31 +0300

    tidy up personal info constants and fetch function

--- src/Home.js
+++ src/Home.js
@@ -1,5 +1,6 @@
-import { useState, useEffect } from "react";
 import BlogList from "./BlogList";
+import useFetch from "./useFetch";
+
 function getAge(dateString) {
     var today = new Date();
     var birthDate = new Date(dateString);
@@ -11,35 +12,21 @@ function getAge(dateString) {
     return age;
 }
 const title = 'Welcome to my blog!';
-const department = 'Computer Engineer';
-const person = {name:'İlhan Bayramoğlu',country:'Turkey'};
-const link = 'https://github.com/ilhan0909';
+const person = {name:'İlhan Bayramoğlu',country:'Turkey',department:'Computer Engineer',link:'https://github.com/ilhan0909'};
+
 
 const Home = () => {
-    const [blogs, setBlogs] = useState(null);
-    const [isPending, setIsPending] =useState(true);
-
-     
-    useEffect(() => {
-        fetch('http://localhost:8000/blogs')
-            .then(res => {
-                return res.json();
-            })
-            .then(data => {
-                setBlogs(data);
-                setIsPending(false);
-            });        
-    }, []);
-
+    const { data,isPending,error } = useFetch('http://localhost:8000/blogs');
     
     return ( 
         <div className="home">            
         <h1>{ title }</h1>
-        <h2>I am a graduate {department} </h2>
-        <h3>My name is {person.name}, i am {getAge("1998/06/25")} years old and i am from {person.country}.</h3>
-        <a href={link}>Check My Github Profile</a>     
+        <h2>My name is {person.name}, i am {getAge("1998/06/25")} years old and i am from {person.country}.</h2>
+        <h3>I am a graduate {person.department} </h3>
+        <a href={person.link}>Check My Github Profile</a>
+            { error && <div>{ error }</div> }     
             { isPending && <div>Loading...</div> }
-            {blogs && <BlogList blogs={blogs} title="All Blogs are here"/>}
+            { data && <BlogList blogs={data} title="All Blogs are here"/> }
         </div>
      );
 }
