f973d729926677fd72955df0c502f3181341bab3
İlhan Bayramoğlu <bayramogluilhan09@gmail.com>
2022-04-23 16:45:10 +0300

    update blog preview list

--- src/BlogList.js
+++ src/BlogList.js
@@ -1,16 +1,14 @@
-const BlogList = ({ blogs, title }) => {
-    // const blogs = props.blogs;
-    // const title = props.title;
-    // console.log(blogs);
+import { Link } from 'react-router-dom';
 
+const BlogList = ({ blogs }) => {
     return (
         <div className="blog-list">
-            <h2>{title}</h2>
             {blogs.map(blog => (
                 <div className="blog-preview" key={blog.id} >
-                    <h2>{blog.title}</h2>
-                    <h4>Written by {blog.author}</h4>
-                    <p>{blog.body}</p>
+                    <Link to={`/blogs/${blog.id}`}>
+                        <h2>{blog.title}</h2>
+                        <p>Written by {blog.author}</p>
+                    </Link>
                 </div>
             ))}
         </div>
