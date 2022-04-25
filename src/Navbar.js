95533e95df9ebfcab05473477be72f3a189b73dd
İlhan Bayramoğlu <bayramogluilhan09@gmail.com>
2022-04-25 12:04:10 +0300

    changed new blog navbar color

--- src/Navbar.js
+++ src/Navbar.js
@@ -8,7 +8,7 @@ const Navbar = () => {
                 <Link to="/">Home</Link>
                 <Link to="/create" style={{
                     color: "white",
-                    backgroundColor: "#f1356d",
+                    backgroundColor: "#7735f1",
                     borderRadius: '8px'
                 }}>New Blog</Link>
             </div>
